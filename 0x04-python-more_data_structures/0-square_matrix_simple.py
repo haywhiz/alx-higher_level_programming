81491bee43800a6cf76f00a4ded936324759ff1a
Haywhiz <babarindeademola@gmail.com>
2022-07-06 21:59:48 -0700

    task_done

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    if (matrix):
+
+        new_matrix = []
+
+        for row in matrix:
+
+            new_matrix.append(list(map(lambda x: x * x, row)))
+
+        return (new_matrix)
