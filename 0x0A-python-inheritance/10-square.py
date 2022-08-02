f07a79e9042f27e050e5253c6d2587dbe57d72ab
Haywhiz <babarindeademola@gmail.com>
2022-08-02 14:30:43 +0100

    correction_task10and11

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,8 +10,14 @@ class Square(Rectangle):
         Implementing the class Square
     '''
     def __init__(self, size):
+        '''
+        Instantiation with size: def __init__(self, size)
+        '''
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        '''
+        area() method must be implemented '''
+        return(self.__size ** 2)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -26,6 +26,9 @@ class Rectangle(BaseGeometry):
         Implements a rectangle
     '''
     def __init__(self, width, height):
+        '''
+        Instantiation with width and height: def __init__(self, width, height)
+        '''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
@@ -33,9 +36,15 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        '''
+        implement area() method
+        '''
         return (self.__width * self.__height)
 
     def __str__(self):
+        '''
+        print() should print, and str() should return
+        '''
         return ("[{}] {}/{}".format(type(self).__name__,
                                     self.__width, self.__height))
 
@@ -45,8 +54,16 @@ class Square(Rectangle):
         Implementing the class Square
     '''
     def __init__(self, size):
+        '''
+        Instantiation with size: def __init__(self, size)
+        '''
+
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        '''
+        implement area() method
+        '''
+        return(self.__size ** 2)
