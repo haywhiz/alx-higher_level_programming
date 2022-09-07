4c950339236fcd0c70cedf7428da83bb4215ace7
Haywhiz <babarindeademola@gmail.com>
2022-09-07 05:56:51 +0100

    lists_all_rows

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,1 +1,5 @@
 -- script that lists all rows of the table
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
+USE hbtn_0c_0
+SELECT COUNT (*) first_table
