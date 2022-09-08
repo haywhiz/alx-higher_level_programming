9dd79197cd01cbdcd3c644e228cccd4956d9150e
Haywhiz <babarindeademola@gmail.com>
2022-09-08 01:22:27 +0100

    lowest_score

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,1 +1,6 @@
--- script that removes all records with a score <= 5 in the table second_table 
+-- Delete Record from a table
+-- a script that removes all records with a score <= 5 in the table second_table
+DELETE FROM
+    second_table
+WHERE
+    score <= 5;
