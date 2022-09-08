9c67cbdeb743f4bfac30abdce23f90ce70a96fad
Haywhiz <babarindeademola@gmail.com>
2022-09-08 01:21:18 +0100

    select_the_best_and_cheating_is_bad

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,1 +1,8 @@
--- script that updates the score of Bob to 10 in the table second_table.
+-- Update record in a table
+-- a script that updates the score of Bob to 10 in a table
+UPDATE
+    second_table
+SET
+    score=10
+WHERE
+    name='Bob';
