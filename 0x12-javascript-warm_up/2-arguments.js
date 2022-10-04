bff79ce9a1c0272bae334305dce24339ecedeead
Haywhiz <babarindeademola@gmail.com>
2022-10-04 02:13:35 +0100

    fix_01

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,6 @@ if (myArgs === 2) {
   console.log('No argument');
 } else if (myArgs === 3) {
   console.log('Argument found');
-} else{
-	console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
